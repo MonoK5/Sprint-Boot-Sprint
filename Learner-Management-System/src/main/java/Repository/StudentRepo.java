804267471fc7ce03ef954672d4000b8f8e50a632
Earthchild2 <nthabiseng.fakude2@gmail.com>
2025-06-13 12:08:46 +0200

    moved folder

--- Learner-Management-System/src/main/java/Repository/StudentRepo.java
+++
@@ -1,9 +0,0 @@
-package Repository;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StudentRepo extends CrudRepository <Student,Long>{
-
-}
