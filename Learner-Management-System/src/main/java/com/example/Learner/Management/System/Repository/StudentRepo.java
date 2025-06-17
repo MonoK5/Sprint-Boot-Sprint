389773aa7be69eea5d301b8d95bd158cd2a8d418
Earthchild2 <nthabiseng.fakude2@gmail.com>
2025-06-17 09:32:56 +0200

    no changes

--- Learner-Management-System/src/main/java/com/example/Learner/Management/System/Repository/StudentRepo.java
+++ Learner-Management-System/src/main/java/com/example/Learner/Management/System/Repository/StudentRepo.java
@@ -4,7 +4,13 @@ import com.example.Learner.Management.System.Entities.Student;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+
 @Repository
 public interface StudentRepo extends CrudRepository <Student, Long>{
+    List<Student> findByName(String name);
+    List<Student> findByScoreGreaterThan(int score);
+    List<Student> findByGrade(int grade);
 
 }
